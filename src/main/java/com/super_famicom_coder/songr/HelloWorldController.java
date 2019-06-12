b79ceeb4e0fe7e0b70f952cf6281d7da41ed60b5
Charles Clemens <cclemensjr@gmail.com>
2019-06-12 10:23:13 -0700

    Added hello route

--- src/main/java/com/super_famicom_coder/songr/HelloWorldController.java
+++ src/main/java/com/super_famicom_coder/songr/HelloWorldController.java
@@ -1,4 +1,14 @@
 package com.super_famicom_coder.songr;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String getHelloWorld() {
+        return "Hello, World!";
+    }
+
 }
