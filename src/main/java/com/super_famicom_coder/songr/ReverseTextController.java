5c9e48d1043eb5ef93a39b92a03508a401fca70a
Charles Clemens <cclemensjr@gmail.com>
2019-06-12 10:47:10 -0700

    Added default reverse route handler

--- src/main/java/com/super_famicom_coder/songr/ReverseTextController.java
+++ src/main/java/com/super_famicom_coder/songr/ReverseTextController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReverseTextController {
 
+    @GetMapping("/reverse")
+    public String getReverse() {
+        return "Welcome to the reverse page!";
+    }
 }
