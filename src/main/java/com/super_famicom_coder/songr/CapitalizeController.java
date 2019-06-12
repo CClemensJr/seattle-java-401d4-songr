e155859b2a2e72b87de7726dfd906711a497afe4
Charles Clemens <cclemensjr@gmail.com>
2019-06-12 10:37:00 -0700

    Added capitalization handlers

--- src/main/java/com/super_famicom_coder/songr/CapitalizeController.java
+++ src/main/java/com/super_famicom_coder/songr/CapitalizeController.java
@@ -1,5 +1,19 @@
 package com.super_famicom_coder.songr;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CapitalizeController {
+
+    @GetMapping("/capitalize")
+    public String getCapitalize() {
+        return "Welcome to the Capitalize page!";
+    }
+
+    @GetMapping("/capitalize/{text}")
+    public String getCapitalizeText(@PathVariable String text) {
+        return text.toUpperCase();
+    }
 }
