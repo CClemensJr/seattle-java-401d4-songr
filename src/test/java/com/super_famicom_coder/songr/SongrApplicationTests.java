62c4c7787babbe3b4be4626e86563b831bc70a27
Charles Clemens <cclemensjr@gmail.com>
2019-06-12 12:00:13 -0700

    Tested routes

--- src/test/java/com/super_famicom_coder/songr/SongrApplicationTests.java
+++ src/test/java/com/super_famicom_coder/songr/SongrApplicationTests.java
@@ -1,16 +1,48 @@
 package com.super_famicom_coder.songr;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import javax.servlet.ServletContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { CapitalizeController.class, ReverseTextController.class })
+@WebAppConfiguration
 public class SongrApplicationTests {
+	@Autowired
+	private WebApplicationContext wac;
+	private MockMvc mockmvc;
 
-	@Test
-	public void contextLoads() {
+	@Before
+	public void setup() throws Exception {
+		this.mockmvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
 	}
 
+	@Test
+	public void canCapitalizeText() throws Exception {
+		this.mockmvc
+				.perform(get("/capitalize/{sentence}", "I ate a walnut"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void canReverseText() throws Exception {
+		this.mockmvc
+				.perform(get("/reverse")
+						.param("sentence", "I ate a walnut"))
+				.andExpect(status().isOk());
+	}
 }
