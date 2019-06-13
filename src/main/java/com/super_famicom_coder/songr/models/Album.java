80dd883cc1f0c390a247cd5709bb22c013507a7b
Charles Clemens <cclemensjr@gmail.com>
2019-06-13 10:15:56 -0700

    Added annotations

--- src/main/java/com/super_famicom_coder/songr/models/Album.java
+++ src/main/java/com/super_famicom_coder/songr/models/Album.java
@@ -1,13 +1,30 @@
 package com.super_famicom_coder.songr.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected long id;
+
+    @NotBlank(message = "An album title is required")
     protected String title;
+
+    @NotBlank(message = "An artist is required")
     protected String artist;
+
+    @NotNull(message = "A song count is required")
     protected int songCount;
+
+    @NotNull(message = "A song length is required")
     protected int songLength;
+
+    @NotBlank(message = "An image source url is required")
     protected String imageUrl;
 }
