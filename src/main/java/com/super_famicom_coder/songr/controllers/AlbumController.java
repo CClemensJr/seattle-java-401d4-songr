dda0fe482dad6441e5555f8a017149d3a9400885
Charles Clemens <cclemensjr@gmail.com>
2019-06-13 11:48:12 -0700

    Added post method to controller

--- src/main/java/com/super_famicom_coder/songr/controllers/AlbumController.java
+++ src/main/java/com/super_famicom_coder/songr/controllers/AlbumController.java
@@ -5,7 +5,11 @@ import com.super_famicom_coder.songr.models.repositories.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AlbumController {
@@ -23,7 +27,18 @@ public class AlbumController {
     }
 
     @GetMapping("/add-album")
-    public String addAlbum() {
+    public String showAlbumForm() {
         return "addAlbum";
+    }
+
+    @PostMapping("/add-album")
+    public String newAlbum(@Valid Album a, BindingResult r, Model m) {
+        if (r.hasErrors()) return "addAlbum";
+
+        albumRepository.save(a);
+
+        m.addAttribute(("albums", albumRepository.findAll()));
+
+        return "allAlbums";
     }
 }
