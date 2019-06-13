9e8ab4571bedcb7e0063fb948a692b11efdf3470
Charles Clemens <cclemensjr@gmail.com>
2019-06-13 10:08:31 -0700

    Added properties

--- src/main/java/com/super_famicom_coder/songr/models/Album.java
+++ src/main/java/com/super_famicom_coder/songr/models/Album.java
@@ -1,5 +1,13 @@
 package com.super_famicom_coder.songr.models;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Album {
+    protected long id;
+    protected String title;
+    protected String artist;
+    protected int songCount;
+    protected int songLength;
+    protected String imageUrl;
 }
