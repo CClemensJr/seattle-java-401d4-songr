4aabf30b8b76ae25bc0c95eb8ac700178d0c7b1a
Charles Clemens <cclemensjr@gmail.com>
2019-06-13 10:47:37 -0700

    Added show all method to controller

--- src/main/java/com/super_famicom_coder/songr/controllers/AlbumController.java
+++ src/main/java/com/super_famicom_coder/songr/controllers/AlbumController.java
@@ -1,4 +1,24 @@
 package com.super_famicom_coder.songr.controllers;
 
+import com.super_famicom_coder.songr.models.Album;
+import com.super_famicom_coder.songr.models.repositories.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AlbumController {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @GetMapping("/albums")
+    public String showAllAlbums(Model m) {
+        Iterable<Album> albums = albumRepository.findAll();
+
+        m.addAttribute("albums", albums);
+
+        return "allAlbums";
+    }
 }
