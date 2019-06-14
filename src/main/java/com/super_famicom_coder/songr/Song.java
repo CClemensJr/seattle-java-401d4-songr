30faeb0f3646c3d2abe1ff24fd8eca1dab8d5f89
Charles Clemens <cclemensjr@gmail.com>
2019-06-14 10:15:48 -0700

    Added Song properties

--- src/main/java/com/super_famicom_coder/songr/Song.java
+++ src/main/java/com/super_famicom_coder/songr/Song.java
@@ -1,4 +1,26 @@
 package com.super_famicom_coder.songr;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Album album;
+
+    @NotBlank(message = "An album title is required")
+    private String title;
+
+    @NotNull(message = "A length is required")
+    private int length;
+
+    @NotNull(message = "A track number is required")
+    private int trackNumber;
+
+
 }
