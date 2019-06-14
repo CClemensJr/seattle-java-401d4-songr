0852252020313acd9a6034d8082b5a1342a17346
Charles Clemens <cclemensjr@gmail.com>
2019-06-14 10:46:17 -0700

    Created method signatures for seeding db

--- src/main/java/com/super_famicom_coder/songr/SongrApplication.java
+++ src/main/java/com/super_famicom_coder/songr/SongrApplication.java
@@ -3,6 +3,8 @@ package com.super_famicom_coder.songr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @EnableAutoConfiguration
 @SpringBootApplication
@@ -10,6 +12,21 @@ public class SongrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SongrApplication.class, args);
+	}
+
+	// Inspired by https://dzone.com/articles/how-to-create-a-database-seeder-in-spring-boot
+	@EventListener
+	public void seedDB(ContextRefreshedEvent event) {
+		seedAlbums();
+		seedSongs();
+	}
+
+	private void seedAlbums() {
+
+	}
+
+	private void seedSongs() {
+
 	}
 
 }
